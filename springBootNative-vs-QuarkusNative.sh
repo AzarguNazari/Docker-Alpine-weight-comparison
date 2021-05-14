8e7584a954f38e9dedb0cb236fa182f2bd299c93
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-05-14 16:19:55 +0200

    quarkus native vs spring boot native

---
+++ springBootNative-vs-QuarkusNative.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build
+#(cd ./frameworks/quarkus/ && sh ./build.sh)
+#(cd ./frameworks/spring-boot-native-image/ && sh ./build.sh)
+
+# Run
+#docker run -d -p 8081:8080 --name quarkus framework-quarkus
+#docker run -d -p 8084:9090 --name spring-boot framework-spring-boot:native
+
+#for i in {0..1000}
+#  do
+#     curl localhost:8081/hello
+#     curl localhost:8084/hello
+#     sleep 0.01
+# done
